8a6d29fb133faa9de0464fe91dc0249933f1f241
fraantic <fraaantic@gmail.com>
2025-07-25 22:09:31 +0800

    nav bar basic done

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,9 +1,41 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import "./navBar.scss"
+
+//Images
+import githubIcon from "../../../public/CompanyIcons/github.svg"
 
 const NavBar = () => {
   return (
-    <div>
-      boo
+    <div className="navbar-parent">
+      <div className="navbar-left-parent">
+        <Image src={githubIcon} width={40} height={40} alt="The logo of one inch of love racing otherly known as OIL Racing"  className="navbar-logo-image" />
+        <div className="navbar-link-container">
+          <Link href="/" className="navbar-link">Home</Link>
+          <Link href="/team" className="navbar-link">Team</Link>
+          <Link href="/liveries" className="navbar-link">Liveries</Link>
+          <Link href="/experience" className="navbar-link">Experience</Link>
+        </div>
+      </div>
+      <div className="navbar-right-parent">
+        <div className="navbar-socials-container">
+          <Link href="https://github.com/fraantic/OIL-Racing-Website" className="navbar-github-icon">
+            <Image className="navbar-github-icon-image" src={githubIcon} width={32} height={32} alt="The github company logo that when clicked goes to the github link of the project"/>
+          </Link>
+          <Link href="https://github.com/fraantic/OIL-Racing-Website" className="navbar-github-icon">
+            <Image src={githubIcon} width={32} height={32} alt="The github company logo that when clicked goes to the github link of the project"/>
+          </Link>
+          <Link href="https://github.com/fraantic/OIL-Racing-Website" className="navbar-github-icon">
+            <Image src={githubIcon} width={32} height={32} alt="The github company logo that when clicked goes to the github link of the project"/>
+          </Link>
+          <Link href="https://github.com/fraantic/OIL-Racing-Website" className="navbar-github-icon">
+            <Image src={githubIcon} width={32} height={32} alt="The github company logo that when clicked goes to the github link of the project"/>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default NavBar
